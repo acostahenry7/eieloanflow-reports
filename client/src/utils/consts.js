571bfd24c0d95f1b5bc6e3eaafe842f5684e93bd
Henry Acosta <acostahenry7@github.com>
2023-10-08 21:40:20 -0400

    prod-build

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,6 +1,6 @@
 let config = {
   server: {
-    url: "https://10.202.8.122:3001/api",
+    url: "http://10.202.8.122:3001/api",
     // url: "https://7h9wr08d-5000.use2.devtunnels.ms/api",
   },
 };
