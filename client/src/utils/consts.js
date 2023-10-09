77a288ddccee22aed3e2cf71597524feec8dcc30
Henry Acosta <acostahenry7@github.com>
2023-10-08 22:10:33 -0400

    prod-build

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -3,6 +3,7 @@ let config = {
     // url: "https://10.202.8.122:3001/api",
     url: "https://eieloanflow-reports.whelastic.net/api",
     // url: "https://7h9wr08d-5000.use2.devtunnels.ms/api",
+    //
   },
 };
 
