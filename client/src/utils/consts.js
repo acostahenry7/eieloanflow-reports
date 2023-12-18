9d08f32e073c2c09410208c8d3d2e42755bd9fc8
Henry Acosta <acostahenry7@github.com>
2023-12-18 00:52:28 -0400

    12-18-speed-booster

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -2,8 +2,8 @@ let config = {
   server: {
     // url: "http://162.219.247.180:3001/api",
     // url: "http://node84051-devfinance-clonetest.whelastic.net/api",
-    // url: "http://eieloanflow-reports.whelastic.net/api",
-    url: "http://localhost:3001/api",
+    url: "http://eieloanflow-reports.whelastic.net/api",
+    // url: "http://localhost:3001/api",
   },
 };
 
