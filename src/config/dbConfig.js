d858bc44119601984d38f0887fce5f43e9023ca6
Henry Acosta <acostahenry7@github.com>
2024-01-01 10:25:18 -0400

    account-and-export-reports

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,23 +1,7 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "postgres",
-  DB: "finance",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
-
-//
-
 // module.exports = {
-//   HOST: "10.202.5.39",
-//   USER: "webadmin",
-//   PASSWORD: "EDJKNOSOj6",
+//   HOST: "localhost",
+//   USER: "postgres",
+//   PASSWORD: "postgres",
 //   DB: "finance",
 //   dialect: "postgres",
 //   pool: {
@@ -27,6 +11,22 @@ module.exports = {
 //     idle: 10000,
 //   },
 // };
+
+//
+
+module.exports = {
+  HOST: "10.202.5.39",
+  USER: "webadmin",
+  PASSWORD: "EDJKNOSOj6",
+  DB: "finance",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
 
 // module.exports = {
 //   HOST: "node77274-devfinance.whelastic.net",
