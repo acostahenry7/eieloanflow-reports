e08010873d80defc460b46985d283c253556168b
Henry Acosta <acostahenry7@github.com>
2024-06-09 01:11:00 -0400

    db-change

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -14,19 +14,19 @@
 
 //
 
-module.exports = {
-  HOST: "10.202.5.39",
-  USER: "webadmin",
-  PASSWORD: "EDJKNOSOj6",
-  DB: "finance",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
+// module.exports = {
+//   HOST: "10.202.5.39",
+//   USER: "webadmin",
+//   PASSWORD: "EDJKNOSOj6",
+//   DB: "finance",
+//   dialect: "postgres",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
 
 // module.exports = {
 //   HOST: "node77274-devfinance.whelastic.net",
@@ -46,20 +46,20 @@ module.exports = {
 //   },
 // };
 
-// module.exports = {
-//   HOST: "node84104-financedb.whelastic.net",
-//   USER: "webadmin",
-//   PASSWORD: "EDJKNOSOj6",
-//   DB: "finance",
-//   dialect: "postgres",
-//   // dialectOptions: {
-//   //   useUTC: false, // for reading from database
-//   // },
-//   //timezone: "-04:00",
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
+module.exports = {
+  HOST: "10.202.9.7",
+  USER: "webadmin",
+  PASSWORD: "EDJKNOSOj6",
+  DB: "finance",
+  dialect: "postgres",
+  // dialectOptions: {
+  //   useUTC: false, // for reading from database
+  // },
+  //timezone: "-04:00",
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
